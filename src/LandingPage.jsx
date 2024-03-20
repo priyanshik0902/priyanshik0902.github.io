8fda1c3d5331fd77d7efd11676db48b966393974
priyanshik0902 <latakotian09@gmail.com>
2024-03-20 19:44:50 +0530

    first

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div >
+    <h1 className="text-3xl font-bold underline">
     Contact ojojojoj
     </div>
   )
+}
 }
 
 export default LandingPage
