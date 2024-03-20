34cc90b33d8e402de4758a15f282f58517ef635c
priyanshik0902 <latakotian09@gmail.com>
2024-03-20 20:21:38 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+build: {
+    rollupOptions: {
+      external: ['react-icons/fa'], // Add react-icons/fa to external dependencies
+    },
+  },
   plugins: [react()],
   base:"/"
 })
